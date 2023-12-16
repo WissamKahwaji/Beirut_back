a9a7e9e1e291762f711207e0dd2e81c173f2981a
WissamKahwaji <kahwajiwisam@gmail.com>
2023-12-16 18:48:26 +0400

    egf

--- routes/about_routes.js
+++ routes/about_routes.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.get("/", getAboutData);
 router.post("/", addAboutData);
-router.put("/", editAboutData);
+router.put("/:id", editAboutData);
 
 export default router;
